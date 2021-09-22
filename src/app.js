45810642efa43416791a809f9ec16d77cbe6fd71
unknown <david.hughes@landz.co.uk>
2021-09-22 13:08:20 +0100

    Add about text

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const forecast = require('./utils/forecast')
 const app = express()
 const port = process.env.PORT || 3000
 
+
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
